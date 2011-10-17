08398516383f7d4afbd78c89d2431573d87f7aa2
Tal Rotbart <redbeard@gmail.com>
2011-10-17 15:29:48 +1100

    Time NTP dep

--- time.rb
+++ time.rb
@@ -1,6 +1,28 @@
 dep 'ntp.managed'
 
+dep 'ntp installed' do
+  requires_when_unmet 'ntp.managed'
+  
+  met? {
+    which 'ntpdate'
+  }
+  
+  meet {
+    sudo 'chkconfig ntpd on'
+  }
+end
+
+dep 'system clock synchronised' do
+  requires 'ntp installed'
+  
+  meet {
+    sudo 'ntpdate pool.ntp.org'
+  }
+end
+
 dep 'ntp service running' do
+  requires_when_unmet 'ntp installed', 'system clock synchronised'
+  
   met? {
     sudo "service ntpd status" # !( /ntpd: unrecognized service/ =~ )
   }
