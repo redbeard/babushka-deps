1c7ce9022ad4e8232165023361a27ed994a4f417
Tal Rotbart <redbeard@gmail.com>
2011-10-17 15:19:54 +1100

    Time NTP dep

--- time.rb
+++ time.rb
@@ -1,12 +1,8 @@
 dep 'ntp.managed'
 
 dep 'ntp service running' do
-  def ntp_is_configued?
-    !(grep /ntpd: unrecognized service/, `service --status-all | grep ntpd`)
-  end
-  
   met? {
-    ntp_is_configured?
+    !(grep /ntpd: unrecognized service/, `service --status-all | grep ntpd`)
   }
   
   meet {
