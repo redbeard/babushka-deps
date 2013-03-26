5d300d516d5af3427a692ef38bb27c41fc1edbaa
Tal Rotbart <redbeard@gmail.com>
2013-03-26 15:23:36 +1100

    Change to use unmeetable! instead of unmeetable.

--- github.rb
+++ github.rb
@@ -1,10 +1,10 @@
 dep 'ssh key exists' do
   met? { "#{ENV['HOME']}/.ssh/id_rsa.pub".p.exists? }
-  meet { unmeetable "You must create SSH key for GitHub manually!" }
+  meet { unmeetable! "You must create SSH key for GitHub manually!" }
 end
 
 dep 'github known host' do
   met? { !shell('ssh-keyscan github.com').empty? }
-  meet { unmeetable "You must authorize GitHub manually by SSHing to it once." }
+  meet { unmeetable! "You must authorize GitHub manually by SSHing to it once." }
 end
 
