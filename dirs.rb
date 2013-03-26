d456704ca9adf6c888cda1265df403b11ffc2b3f
Tal Rotbart <redbeard@gmail.com>
2013-03-26 15:16:58 +1100

    Refactoring directory deps to match new dep params.

--- dirs.rb
+++ dirs.rb
@@ -23,6 +23,7 @@ dep( "directory", :dir, :as_sudo ) do
 
   meet { 
     unless does_exist?
+      log_ok "As sudo? #{as_sudo ? 'Yes' : 'No'}"
       directory.mkpath unless as_sudo
       sudo "mkdir -p '#{dir.to_s}'" if as_sudo
       unmeetable! "Couldn't create directory: '#{dir}'" unless does_exist?
