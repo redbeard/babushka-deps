0c7920601655e21704fefe2a27e9810d60dd8ace
Tal Rotbart <redbeard@gmail.com>
2013-03-26 14:07:49 +1100

    Refactoring directory deps to match new dep params.

--- dirs.rb
+++ dirs.rb
@@ -1,5 +1,42 @@
 require 'fileutils'
 
+dep 'directory', :dir, :as_sudo
+  as_sudo.default(false)
+
+  def directory
+    dir.p
+  end
+
+  def does_exist?  
+     directory.exists?
+  end
+  
+  def is_correct_ownership? 
+    (does_exist? && directory.owned?)
+  end
+  
+  met? { 
+    log "Directory '#{dir}' #{does_exist? ? "exists #{ is_correct_ownership? ? 'and is' : "but isn't" } owned by the user." : "doesn't exist"}"
+
+    does_exist? && is_correct_ownership?
+  }
+
+  meet { 
+    unless does_exist?
+      directory.mkpath unless as_sudo
+      sudo "mkdir -p '#{dir.to_s}'" if as_sudo
+      unmeetable! "Couldn't create directory: '#{dir}'" unless does_exist?
+      log_ok "Directory '#{dir}' created successfully."
+    end
+    
+    unless is_correct_ownership? 
+      sudo "chown -R #{Process.uid}:#{Process.gid} #{dir}"
+      unmeetable "Couldn't change permission for: '#{dir}'" unless is_correct_ownership?
+      log_ok "Directory '#{dir}' now owned by user."
+    end
+  }  
+end
+
 meta 'dir' do
   accepts_value_for :var_name, :default_var_name
   accepts_value_for :dir, :default_dir
